c1abbbe346141e20af3871f9461b3e557232c5d7
safuvan12 <muhammedsafuvan2506@gmail.com>
2023-10-02 12:38:42 +0530

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,16 +21,16 @@
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
  
 
-//   final String title;
+  final String title;
 
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
@@ -42,13 +42,13 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-//   @override
-//   Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
    
-//     return Scaffold(
-//       appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
        
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
        
         title: Text(widget.title),
       ),
